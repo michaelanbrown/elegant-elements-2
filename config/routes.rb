aadcb3b3724e6bfb56d48b196cdd3ff72ec34b70
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 16:24:42 -0700

    routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   
   resources :product_orders
   resources :orders
-  resources :products
-  resources :addresses
-  resources :customers
+  resources :products, only: [:index, :show]
+  resources :addresses, only: [:index, :show, :create, :update]
+  resources :customers, only: [:index, :show, :create]
+
+  post "/login", to: "sessions#create" 
+  delete "/logout", to: "sessions#destroy"
+  get "/authorized_user", to: "customers#show"
+  post "/checkout", to: "charges#create"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

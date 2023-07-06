10ab4557afdccdf13d02557d027fd41ed9313e5e
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 16:10:02 -0700

    order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :customer_id, :total, :shipping, :status, :address_id
+  attributes :id, :customer_id, :total, :shipping, :status, :address_id, :editable, :created_at, :updated_at
   has_many :products
   has_many :product_orders
 
@@ -9,5 +9,21 @@ class OrderSerializer < ActiveModel::Serializer
   def total
     @product_total = object.product_orders.map{|o| o.quantity * Product.find(o.product_id).price}
     @product_total.sum + object.shipping
+  end
+
+  def editable
+    if object.status == "in progress" || (object.status == "submitted" && Time.at(created_at.to_i) > Time.at(Time.now-1.day.to_i))
+      return true
+    else
+      return false
+    end
+  end
+
+  def created_at
+    object.created_at.strftime("%B %Y")
+  end
+
+  def updated_at
+    object.created_at.strftime("%B %Y")
   end
 end

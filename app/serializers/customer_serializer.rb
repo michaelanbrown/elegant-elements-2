483688ec07a084e525cd3e07667b796bd3588244
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 14:33:42 -0700

    customer serizalizer done

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,20 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :username, :password_digest, :products, :in_progress_product_count, :admin
+
+  has_many :orders
+  has_many :addresses
+
+  def products
+    @order_ids = object.orders.map{ |o| o.id }
+    @product_arrays = @order_ids.map { |o| Product.where(order_id: o)}
+    @prods = []
+    @product_arrays.map{|p| p.map{|p| @prods.push(p)}}
+    @prods.each{|p| p.jewelry = p.jewelry.capitalize}.uniq{|p| p.jewelry && p.customization.personalization}
+  end
+
+  def in_progress_product_count
+    @in_progess_order = Order.where(status: "in progress", customer_id: object.id)
+    @in_progess_order.map{ |o| o.id }
+    Product.where(order_id: @in_progess_order).count
+  end
 end

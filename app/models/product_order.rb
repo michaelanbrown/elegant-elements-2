278e22ad8e90a5c186eb4d7ba061b5cde2f4183c
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 14:13:15 -0700

    word personalization validation

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,4 +1,32 @@
 class ProductOrder < ApplicationRecord
     belongs_to :product
     belongs_to :order
+
+    validate :word_personalization
+
+    private
+
+    def word_personalization
+        return if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" ").to_i < 0)
+
+        if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" "))
+            errors.add(:personalization, "Cannot be multiple words")
+        end
+    end
+
+    def phrase_personalization
+        return if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" ").to_i < 0)
+
+        if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" "))
+            errors.add(:personalization, "Cannot be multiple words")
+        end
+    end
+
+    def date_personalization
+        return if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" ").to_i < 0)
+
+        if ((product.jewelry == "Necklace with a Word" || product.jewelry == "Bracelet with a Word" || product.jewelry == "Keychain with a Word") && personalization.index(" "))
+            errors.add(:personalization, "Cannot be multiple words")
+        end
+    end
 end

a2d2a685fbcca67d760bce479a833516f77f15b0
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 16:11:41 -0700

    serializers complete

--- app/serializers/product_order_serializer.rb
+++ app/serializers/product_order_serializer.rb
@@ -1,3 +1,6 @@
 class ProductOrderSerializer < ActiveModel::Serializer
   attributes :id, :personalization, :quantity, :order_id, :product_id
+
+  belongs_to :order
+  belongs_to: product
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :jewelry, :price, :stripe_key
+
+  has_many :orders
+  has_many :product_orders
 end

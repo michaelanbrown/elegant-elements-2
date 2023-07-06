ad958a777306ab97bac653ab248d9669ef4c1a72
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 16:04:45 -0700

    order total auto calculated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,5 @@
 class OrdersController < ApplicationController
+    def index 
+        render json: Order.all, status: :ok
+    end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,13 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :customer_id, :total, :shipping, :status, :address_id
+  has_many :products
+  has_many :product_orders
+
+  belongs_to :customer
+  belongs_to :address
+
+  def total
+    @product_total = object.product_orders.map{|o| o.quantity * Product.find(o.product_id).price}
+    @product_total.sum + object.shipping
+  end
 end

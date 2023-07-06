9b5a6cdff910be1044f5ed91c2781696ed6f3f91
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 16:13:46 -0700

    application and address controllers

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,28 @@
 class AddressesController < ApplicationController
+    before_action :find_address, only: [:show, :update]
+    before_action :authorize_customer_on_address, only: [:update, :destroy]
+
+    def index 
+        render json: Address.all, status: :ok
+    end
+
+    def show
+        render json: @address, status: :ok
+    end
+
+    def create
+        order = Address.create!(address_params)
+        render json: order, status: :created
+    end
+
+    def update
+        @address.update!(address_params)
+        render json: @address, status: :accepted
+    end
+
+    private
+
+    def address_params
+        params.permit(:street, :unit, :city, :state, :zip, :name).merge(customer_id: current_customer.id)
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,55 @@
 class ApplicationController < ActionController::API
+
+  before_action :authenticate_customer
+
   include ActionController::Cookies
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def current_customer
+    @current_customer ||= Customer.find_by(id: session[:customer_id])
+  end
+private
+
+  def authenticate_customer
+    render json: { errors: "Not authorized" }, status: :unauthorized unless current_customer
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def find_customization
+    @customization = Customization.find(params[:id])
+  end
+
+  def find_address
+    @address = Address.find(params[:id])
+  end
+
+  def authorize_customer_on_order
+    find_order
+    permitted = @order.customer_id == @current_customer.id || @current_customer.admin == true
+    render json: { errors: "Not authorized" }, status: :unauthorized unless permitted
+  end
+
+  def authorize_customer_on_address
+    find_address
+    permitted = @address.customer.id == @current_customer.id
+    render json: { errors: "Not authorized" }, status: :unauthorized unless permitted
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Record not found" }, status: :not_found
+  end
 
 end

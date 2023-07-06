f02746266ee512680e5e89be015afa72b734210a
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 15:44:17 -0700

    in_progress_product_count works

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :username, :password_digest, :personalizations, :admin, :orders
+  attributes :id, :name, :email, :username, :password_digest, :personalizations, :in_progress_product_count, :admin, :orders
 
   has_many :orders
   has_many :addresses
@@ -10,9 +10,9 @@ class CustomerSerializer < ActiveModel::Serializer
     @personalizations.flatten
   end
 
-  # def in_progress_product_count
-  #   @in_progess_order = Order.where(status: "in progress", customer_id: object.id)
-  #   @in_progess_order.map{ |o| o.id }
-  #   Product.where(order_id: @in_progess_order).count
-  # end
+  def in_progress_product_count
+    @in_progess_order = Order.where(status: "in progress", customer_id: object.id)
+    @in_progess_order.map{ |o| o.id }
+    ProductOrder.where(order_id: @in_progess_order).count
+  end
 end

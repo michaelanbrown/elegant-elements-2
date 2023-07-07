c299d15a12fe5217e0d3cf8ad424199b96f2fbfb
Michaela Brown <m_brownbrown@yahoo.com>
2023-07-06 19:39:31 -0700

    can delete order from cart

--- client/src/components/ProductCartCard.js
+++ client/src/components/ProductCartCard.js
@@ -28,32 +28,30 @@ function ProductCartCard({ product_order, products, order, setOrder, custProduct
             }
     })}
 
-    function deletingProductOrder(deletedProduct) {
-        const deletingProduct = custProducts.filter((prod) => {
-            if (prod.id !== deleteProduct.id) {
-                return deletedProduct
+    function deletingProductOrder(product_order) {
+        const deletingProductOrder = order.product_orders.filter((prod) => {
+            if (prod.id !== product_order.id) {
+                return product_order
             }
         })
-        setCustProducts(deletingProduct)
+        setOrder(deletingProductOrder)
     }
 
     function deleteProduct() {
-        fetch(`products/${currentProduct.id}`, {
+        fetch(`product_orders/${product_order.id}`, {
             method:"DELETE"
         })
         .then(res =>{
           if(res.ok){
-            setOrderTotalAddition(orderTotalAddition)
-            setCurrentProduct({
-                ...currentProduct,
-                quantity: 0
-            })
+            setOrderTotalAddition(orderTotalAddition - (currentProduct.price * product_order.quantity))
+            setCurrentProduct([])
             setProductCount(productCount - 1)
             deletingProductOrder(product_order)
           }
           if (productCount === 0) {
             setOrder({...order,
-                products: []})
+                products: [],
+                product_orders: []})
           }
         })
     }
@@ -63,7 +61,7 @@ function ProductCartCard({ product_order, products, order, setOrder, custProduct
             setProductUpdate({
                 quantity: quantity - 1
             })
-            setOrderTotalAddition(orderTotalAddition- currentProduct.price)
+            setOrderTotalAddition(orderTotalAddition - currentProduct.price)
             setKeepChanges(true)
         }
     }
@@ -87,7 +85,7 @@ function ProductCartCard({ product_order, products, order, setOrder, custProduct
                 <p>Quantity: <input type="button" value="-" onClick={downClick} />
                     {" "}{quantity}{" "}
                 <input type="button" value="+" onClick={upClick}/></p>
-                <p>Item Total: ${currentProduct.price * product_order.quantity + orderTotalAddition}</p>
+                <p>Item Total: ${(currentProduct.price * product_order.quantity) + orderTotalAddition}</p>
                 { keepChanges ? <button onClick={quantityUpdate}>Keep Changes?</button> : null}
                 <br/>
                 <button onClick={deleteProduct}>Remove Item</button>
